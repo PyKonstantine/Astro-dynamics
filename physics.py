abda3144dadd04d8ce2c291db15d90e8ea78b397
cyber_ego <chai.ne.voron1991@gmail.com>
2022-02-14 21:56:49 +0300

    fix collision event(uncrossing objects and impulse derection)

--- physics.py
+++ physics.py
@@ -28,11 +28,15 @@ class Physics(Measurement):
 
     def collision(self, obj, r):
         if r < (self.radius + obj.radius):
+            x1 = ((self.mass - obj.mass) * self.speedx) / (self.mass + obj.mass)
+            y1 = ((self.mass - obj.mass) * self.speedy) / (self.mass + obj.mass)
+            x2 = (2 * self.mass * self.speedx) / (self.mass + obj.mass)
+            y2 = (2 * self.mass * self.speedy) / (self.mass + obj.mass)
+            self.speedx = x1 * 1.3
+            self.speedy = y1 * 1.3
+            obj.speedx = x2 * 1.3
+            obj.speedy = y2 * 1.3
             return True
-            # self.speedx = ((self.mass - obj.mass) * self.speedx) / (self.mass + obj.mass)
-            # self.speedy = ((self.mass - obj.mass) * self.speedy) / (self.mass + obj.mass)
-            # self.speedx += obj.speedx
-            # self.speedy += obj.speedy
 
     def absorption(self, obj):
         if self.mass == max(self.mass, obj.mass):
@@ -61,30 +65,28 @@ class Planet(Physics):
         for obj in planets:
             if obj.name != self.name:
                 r = self.measure_distance(obj.x, obj.y)
-
                 if self.collision(obj, r):
-                    continue
+                    self.absorption(obj)
                 self.gravity(obj.mass, obj.x, obj.y, r)
-                self.collision(obj, r)
-                self.absorption(obj)
 
 
 #  --------------------------------------------objects-init-------------------------------------------------------------
 
 planets = [
     # Star system
-    # Planet("Sun", 5000, 50, (600, 400), (0, 0), (255, 100, 100)),
-    # Planet("mercury", 50, 5, (600, 200), (-2.5, 0), (255, 10, 100)),
-    # Planet("Venus", 60, 6, (600, 900), (1.5, 0), (236, 193, 19)),
-    # Planet("Earth", 100, 10, (1400, 400), (0, -1.3), "blue"),
-    # Planet("Mars", 80, 8, (-600, 400), (0, 1), "red")
+    Planet("Sun", 5000, 50, (600, 400), (0, 0), (255, 100, 100)),
+    Planet("mercury", 50, 5, (600, 200), (-2.5, 0), (255, 10, 100)),
+    Planet("Venus", 60, 6, (600, 900), (1.5, 0), (236, 193, 19)),
+    Planet("Earth", 100, 10, (1400, 400), (0, -1.3), "blue"),
+    Planet("Mars", 80, 8, (-600, 400), (0, 1), "red")
+
     # collate test
-    Planet("P1", 50, 5, (635, 368), (0, 0), "green"),
-    Planet("P1", 50, 5, (435, 365), (0, 0), "blue"),
-    Planet("S1", 100, 10, (412, 556), (0, 0), (255, 100, 100)),
-    Planet("S2", 100, 10, (648, 548), (0, 0), 'red'),
-    Planet("S3", 100, 10, (550, 251), (0, 0), 'yellow'),
-    Planet("S4", 200, 15, (650, 251), (0, 0), 'white'),
-    Planet("P1", 50, 5, (535, 368), (0, 0), "blue"),
-    Planet("P1", 50, 5, (635, 468), (0, 0), "green")
+    # Planet("P1", 50, 5, (635, 368), (0, 0), "green"),
+    # Planet("P1", 50, 5, (435, 365), (0, 0), "blue"),
+    # Planet("S1", 100, 10, (412, 556), (0, 0), (255, 100, 100)),
+    # Planet("S2", 100, 10, (648, 548), (0, 0), 'red'),
+    # # # Planet("S3", 100, 10, (550, 251), (0, 0), 'yellow'),
+    # Planet("S4", 300, 15, (650, 251), (0, 0), 'white'),
+    # Planet("P1", 50, 5, (535, 368), (0, 0), "blue"),
+    # Planet("P1", 50, 5, (635, 468), (0, 0), "green")
           ]
